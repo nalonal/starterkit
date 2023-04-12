4c235f0e163544ea6645f49f2bb10e7a111e49b2
nalonal <rionaldyrizqy@gmail.com>
2023-04-12 10:18:36 +0700

    init

---
+++ install.sh
@@ -0,0 +1,12 @@
+sudo apt update
+sudo apt install lsb-release ca-certificates apt-transport-https software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install docker-ce -y
+mkdir -p ~/.docker/cli-plugins/
+curl -SL https://github.com/docker/compose/releases/download/v2.5.0/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
+chmod +x ~/.docker/cli-plugins/docker-compose
+sudo chmod 666 /var/run/docker.sock
+sudo chmod a+x restart.sh
+docker compose up -d
---
+++ readme.md
@@ -0,0 +1,16 @@
+## Pre-install
+sudo apt get update<br>
+sudo apt install git
+
+## Running As Super User
+sudo su
+
+## Clone Repository
+git clone https://github.com/nalonal/starterkit<br>
+sudo chmod 777 -R starterkit<br>
+cd starterkit<br>
+chmod u+x install.sh<br>
+./install.sh
+
+## Credit
+Script By Nxlonal<br>
